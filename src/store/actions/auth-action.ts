4d6a070f88a546c94e4a71eee2f1b8a9d3f0c268
thanhquisg24 <thanhquisg24@gmail.com>
2022-08-17 17:31:16 +0700

    yy

--- src/store/actions/auth-action.ts
+++ src/store/actions/auth-action.ts
@@ -8,10 +8,25 @@ export interface ILoginPayload {
 export interface ICheckAuthPayload {
   token: string;
 }
-export const dologinSpinner = "user/LOGIN";
+export interface IRefreshTokenSuccessPayload {
+  accessToken: string;
+  refreshToken: string;
+  tokenType: string;
+}
+export const doLoginSpinner = "user/LOGIN";
 export const doLoginRequest = createAction<ILoginPayload>("user/LOGIN_REQUEST");
 export const doLoginSuccess = createAction<IUserEntity>("user/LOGIN_SUCCESS");
 export const doLoginFailure = createAction<string>("user/LOGIN_FAILURE");
+
+export const doLogoutSpinner = "user/LOGOUT";
+export const doLogoutRequest = createAction<undefined>("user/LOGOUT_REQUEST");
+export const doLogoutSuccess = createAction<undefined>("user/LOGOUT_SUCCESS");
+export const doLogoutFailure = createAction<string>("user/LOGOUT_FAILURE");
+
+export const doRefreshTokenSpinner = "user/REFRESH_TOKEN";
+export const doRefreshTokenRequest = createAction<string>("user/REFRESH_TOKEN_REQUEST");
+export const doRefreshTokenSuccess = createAction<IRefreshTokenSuccessPayload>("user/REFRESH_TOKEN_SUCCESS");
+export const doRefreshTokenFailure = createAction<string>("user/REFRESH_TOKEN_FAILURE");
 
 export const checkAuthSpinner = "user/CHECK_AUTH";
 export const checkAuthRequest = createAction<ICheckAuthPayload>("user/CHECK_AUTH_REQUEST");
@@ -19,6 +34,12 @@ export const checkAuthSuccess = createAction<undefined>("user/CHECK_AUTH_SUCCESS
 export const checkAuthFailure = createAction<string>("user/CHECK_AUTH_FAILURE");
 
 export type CombineAuthActionTypes =
+  | ReturnType<typeof doRefreshTokenRequest>
+  | ReturnType<typeof doRefreshTokenSuccess>
+  | ReturnType<typeof doRefreshTokenFailure>
+  | ReturnType<typeof doLogoutRequest>
+  | ReturnType<typeof doLogoutSuccess>
+  | ReturnType<typeof doLogoutFailure>
   | ReturnType<typeof doLoginRequest>
   | ReturnType<typeof doLoginSuccess>
   | ReturnType<typeof doLoginFailure>
--- src/store/sagas/auth-saga.ts
+++ src/store/sagas/auth-saga.ts
@@ -1,5 +1,14 @@
 import { IUserEntity } from "@adapters/entity";
-import { doLoginFailure, doLoginRequest, doLoginSuccess } from "@store/actions/auth-action";
+import {
+  doLoginFailure,
+  doLoginRequest,
+  doLoginSuccess,
+  doLogoutRequest,
+  doLogoutSuccess,
+  doLogoutFailure,
+  doRefreshTokenRequest,
+  doRefreshTokenFailure,
+} from "@store/actions/auth-action";
 import { delay, put, takeLatest } from "redux-saga/effects";
 
 const sampleUser: IUserEntity = {
@@ -15,14 +24,51 @@ function* loginSaga(action: ReturnType<typeof doLoginRequest>): Generator | any 
   try {
     yield delay(5000);
     yield put(doLoginSuccess(sampleUser));
+    yield delay(2000);
+    yield put(doRefreshTokenRequest(sampleUser.refreshToken));
   } catch (error) {
     // console.log("🚀 ~ file: dega-sport-saga.ts ~ line 24 ~ function*fetchSportSaga ~ error", error);
     yield put(doLoginFailure("Login fail!"));
     // notifyMessageError("Fetch Sports fail!");
   }
 }
+
+function* logoutSaga(): Generator | any {
+  try {
+    yield delay(5000);
+    yield put(doLogoutSuccess());
+  } catch (error) {
+    // console.log("🚀 ~ file: dega-sport-saga.ts ~ line 24 ~ function*fetchSportSaga ~ error", error);
+    yield put(doLogoutFailure("Logout fail!"));
+    // notifyMessageError("Fetch Sports fail!");
+  }
+}
+
+function* refreshTokenSaga(action: ReturnType<typeof doRefreshTokenRequest>): Generator | any {
+  // yield call(api);
+  try {
+    yield delay(60000); //interval refresh
+    const rstokenResponse = {
+      accessToken: "aaaaa",
+      refreshToken: "bbbbb",
+      tokenType: "ccccc",
+    };
+    yield put(doRefreshTokenRequest(rstokenResponse.refreshToken));
+  } catch (error) {
+    yield put(doRefreshTokenFailure("Refresh token fail!"));
+    // notifyMessageError("Fetch Sports fail!");
+  }
+}
+
+function* refreshTokenSagaWatcher() {
+  yield takeLatest(doRefreshTokenRequest.type, refreshTokenSaga);
+}
+
 function* loginSagaWatcher() {
   yield takeLatest(doLoginRequest.type, loginSaga);
 }
+function* logoutSagaWatcher() {
+  yield takeLatest(doLogoutRequest.type, logoutSaga);
+}
 
-export const authWatchers = [loginSagaWatcher()];
+export const authWatchers = [loginSagaWatcher(), logoutSagaWatcher(), refreshTokenSagaWatcher()];
